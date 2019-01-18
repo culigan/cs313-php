4a3212ea7657e5293a6a5aedb78e06cf59210614
culigan <culigan@gmail.com>
2019-01-17 19:46:05 -0700

    centering video

--- web/AssignWeek2_1.php
+++ web/AssignWeek2_1.php
@@ -30,7 +30,7 @@
    <button id="but" onclick="click()">Push</button><br />
    The time is:
    <?php
-    print date('H:i:s', $_SERVER['REQUEST_TIME']);
+      print date('H:i:s', $_SERVER['REQUEST_TIME']);
    ?>
 </body>
 </html>
