fb4a95fe3524f8b1e0097207dfe25ea1562863ad
culigan <culigan@gmail.com>
2019-01-23 20:42:20 -0700

    Finishing toughes

--- web/TeamWeek3.php
+++ web/TeamWeek3.php
@@ -11,8 +11,6 @@
 <head>
    <title>CS313</title>
    <meta name="viewport" content="width=device-width, initial-scale1" />
-   <link href="AssignWeek2.css" rel="stylesheet">
-   <script type="text/javascript" src="AssignWeek2.js"></script>
 </head>
 <body>
    <header>CS313 Collin Steel</header>
