7cbf88215b44854a7a9cf0d496955ff3c67275da
culigan <culigan@gmail.com>
2019-01-23 19:50:10 -0700

    Finishing toughes

--- web/TeamWeek3.php
+++ web/TeamWeek3.php
@@ -11,6 +11,7 @@
    
    <?php
       console.log("test1");
+      var_dump($_POST['name']);
       echo $_POST['name'];
       echo "mailto: <a href=$_POST['email']>$_POST['email']</a><br />";
       echo $_POST['radio'];
