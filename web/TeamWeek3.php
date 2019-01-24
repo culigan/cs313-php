1527204418bebfdedff952286f4e4f01ad21452c
culigan <culigan@gmail.com>
2019-01-23 21:38:12 -0700

    Finishing toughes

--- web/TeamWeek3.php
+++ web/TeamWeek3.php
@@ -14,8 +14,8 @@
 <body>
    <header>Team AssignWeek3</header>
    
-   <p>name: <? =$name?></p>
-   <p>email: <? =$email?></p>
+   <p>name: <? =$name ?></p>
+   <p>email: <? =$email ?></p>
    <p>major: <? =$radio ?></p>
    
    
