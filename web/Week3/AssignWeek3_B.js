cd04c5066faabda7b315330d48c84b2cf2d725db
culigan <culigan@gmail.com>
2019-01-25 20:51:26 -0700

    testing php

--- web/Week3/AssignWeek3_B.js
+++ web/Week3/AssignWeek3_B.js
@@ -1,16 +1,18 @@
 // JavaScript source code
-function addToCart(){
-   
-   $.ajax({
-      url: 'http://calm-shelf-84172.herokuapp.com/Week3/AssignWeek3_B.php',
-      method: "post",
-      data: cart,
-      success: function () {
-         alert("Item has been added");
-      }
-   });
-}
 
 function viewCart() {
    window.open("https://calm-shelf-84172.herokuapp.com/AssignWeek3_Cart.php", '_top');
 }
+
+$(document).ready(function () {
+   $("pantsbutton").click(function () {
+      $.ajax({
+         url: 'http://calm-shelf-84172.herokuapp.com/Week3/AssignWeek3_B.php',
+         method: "post",
+         data: cart,
+         success: function () {
+            alert("Item has been added");
+         }
+      });
+   });
+});
