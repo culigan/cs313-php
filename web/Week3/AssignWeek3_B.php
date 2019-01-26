710032b3080bbf46d8bb21af44e4ba9ec3f1ee97
culigan <culigan@gmail.com>
2019-01-25 21:39:02 -0700

    testing php

--- web/Week3/AssignWeek3_B.php
+++ web/Week3/AssignWeek3_B.php
@@ -9,7 +9,7 @@
    <meta charset="utf-8" />
    <title>Browsing Page</title>
   <link href="AssignWeek3.css" rel="stylesheet">
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
    <script>
       $(document).ready(function(){
         $("button").click(function(){
