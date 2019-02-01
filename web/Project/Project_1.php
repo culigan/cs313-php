2eb4c42c8fd02fcb835f7485575948def9cd06e2
culigan <culigan@gmail.com>
2019-02-01 10:26:17 -0700

    adding my project

--- web/Project/Project_1.php
+++ web/Project/Project_1.php
@@ -2,20 +2,26 @@
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
    <meta charset="utf-8" />
-   <title>Shopping Page</title>
+   <title>Add a Recipe</title>
    <link href="Project.css" rel="stylesheet">
-   <script src="AssignWeek3_B.js"></script>
+   <script src="ProjectJS.js"></script>
 </head>
 <body>
-   <header>Checkout</header>
+   <header>Add a Recipe</header>
    <div id='itemdiv'>
-      <form action="AssignWeek3_Confirm.php" method="post">
+      <form id="formid" action="AssignWeek3_Confirm.php" method="post">
          <p></p></br>
-         <input name="street" type="text" required>Street Address</br></br>
-         <input name="city" type="text" required>City</br></br>
-         <input name="state" type="text" required>State</br></br>
-         <input name="zip" type="text" required>Zip</br></br>
-         <p><a href="AssignWeek3_Cart.php">Return to Cart</a></p>
+         <div id="ingred">
+            <select name="amount"  required>
+               <option value="1/2">1/2</option>
+            </select>Amount
+            <select name="meastype" required>
+               <option value="Cup">Cup</option>
+            </select>Measurement Type
+            <input name="ingredient" type="text" required>Ingredient</br></br>
+         </div>
+         <input name="add" type="button" value="Add another Ingredient" onclick="addItem()"></br></br>
+         <textarea name="direct" rows="4" cols="50" required>Enter directions here....</textarea></br></br>
          <input type="submit" value="Submit">
       </form>
    </div>
