d29e957d6c71c746c25a99f0ecf812ca5797d1e8
culigan <culigan@gmail.com>
2019-02-02 11:04:05 -0700

    adding my project

--- web/Project/Project_1.php
+++ web/Project/Project_1.php
@@ -16,12 +16,12 @@
             $dbOpts = parse_url($dbUrl);
 
             $dbHost = $dbOpts["host"];
-            echo $dbHost;
-            /*$dbPort = $dbOpts["port"];
+            $dbPort = $dbOpts["port"];
             $dbUser = $dbOpts["user"];
             $dbPassword = $dbOpts["pass"];
             $dbName = ltrim($dbOpts["path"],'/');
-            $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+            echo $dbHost . $dbName;
+            /*$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION)*/
          }
