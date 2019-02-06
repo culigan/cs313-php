44dcab016cc4c4bf90eba06697a0a137402e27d7
culigan <culigan@gmail.com>
2019-02-05 22:16:48 -0700

    adding my project

--- web/Project/Project_2.php
+++ web/Project/Project_2.php
@@ -73,9 +73,9 @@ catch (PDOException $ex)
                $fromString = " FROM recipes r ";
                $whereString = " where";
                //if(isset($_POST['type0'])){
-                  //$searchString = $searchString . ", f.typename";
-                  $fromString = " inner join FoodType f on r.foodtype_id = f.id ";
-                  $whereString = " f.typename = $_POST['type0']";   
+                  $searchString = $searchString . ", f.typename";
+                  $fromString = $fromString . " inner join FoodType f on r.foodtype_id = f.id ";
+                  //$whereString = $where . " f.typename = $_POST['type0']";   
                //}
                /*if(isset($_POST['mealCat0'])){
                   $searchString = $searchString . ", c.categoryname";
