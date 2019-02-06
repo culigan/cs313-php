bd082bcca9967a884aedccaf8ee161c8ec66871b
culigan <culigan@gmail.com>
2019-02-05 22:25:55 -0700

    adding my project

--- web/Project/Project_2.php
+++ web/Project/Project_2.php
@@ -91,7 +91,7 @@ catch (PDOException $ex)
                   $whereString = $whereString . "and ri.Ingredients like '%sugar%' " . $_POST['ingred'];   
                }
                $searchString = $searchString . $fromString;
-               if($whereString.length > 6)
+               //if($whereString.length > 6)
                   $searchString = $searchString . $whereString
                $searchString = $searchString . ";";
                echo $db->query($searchString);
