6eef8a3e4cc6415d38483ea1db84aeb35345a4cb
culigan <culigan@gmail.com>
2019-02-09 09:05:19 -0700

    adding my project

--- web/Week6/Team06_Display.php
+++ web/Week6/Team06_Display.php
@@ -41,7 +41,9 @@ catch (PDOException $ex)
    $newId = $db->lastInsertId('scriptures_id_seq');*/
    
    if(isset($_POST['topic0'])){
+      echo 'got here'
       $topic = $_POST['topic0'];      
+      echo $topic;
       $inserttop = $db-exec("insert into scripture_topic_link (scripture_id, topics_id) values ( 6, 1);");
       /*//$inserttop->bindValue(':newId', $newId);
       $inserttop->execute();*/
