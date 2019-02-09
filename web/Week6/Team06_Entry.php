d3288a1139005b423a2221898300701d930750ad
culigan <culigan@gmail.com>
2019-02-08 22:24:40 -0700

    adding my project

--- web/Week6/Team06_Entry.php
+++ web/Week6/Team06_Entry.php
@@ -1,32 +1,4 @@
-﻿<?php
-echo "first";
-$db;
-try
-{
-    $dbUrl = getenv('DATABASE_URL');
-    $dbOpts = parse_url($dbUrl);
-
-    $dbHost = $dbOpts["host"];
-    $dbPort = $dbOpts["port"];
-    $dbUser = $dbOpts["user"];
-    $dbPassword = $dbOpts["pass"];
-    $dbName = ltrim($dbOpts["path"],'/');
-
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $sizes = $db->query("SELECT * FROM MeasurementSize;");
-    $types = $db->query("SELECT * FROM MeasurementType;");
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
-echo "test";
-?>
-
+﻿
 <!DOCTYPE html>
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
@@ -40,14 +12,7 @@ echo "test";
       <input name="chpt" type="text" required />Chapter</br></br>
       <input name="verse" type="text" required />Verse</br></br>
       <textarea name="content" type="text" required>Content</textarea></br></br>
-      <?php
-         $count = 0;
-         foreach($db->query('SELECT * FROM topics;') as $row){
-            echo "<input type='checkbox' name='topic" . $count . "' value='" . $row['name'] . "'/>";
-            count++;
-         }
-         
-      ?>
+      
       <button type="submit" value="Submit">Submit</button>
    </form>
 </body>
