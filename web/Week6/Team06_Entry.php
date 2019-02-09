eeecc48493b1c216c09c5858cb29504c2019901d
culigan <culigan@gmail.com>
2019-02-08 22:26:23 -0700

    adding my project

--- web/Week6/Team06_Entry.php
+++ web/Week6/Team06_Entry.php
@@ -1,4 +1,32 @@
-﻿
+﻿<?php
+echo "first";
+$db;
+try
+{
+    $dbUrl = getenv('DATABASE_URL');
+    $dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sizes = $db->query("SELECT * FROM MeasurementSize;");
+    $types = $db->query("SELECT * FROM MeasurementType;");
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+echo "test";
+?>
+
 <!DOCTYPE html>
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
