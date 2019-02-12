6580788e1151dee78ef39357eac6adb93946688b
culigan <culigan@gmail.com>
2019-02-11 22:22:37 -0700

    adding my project

--- web/Project/Project_Edit.php
+++ web/Project/Project_Edit.php
@@ -37,7 +37,7 @@ session_start();
    <header>Search Recipes</header>
    <div id='itemdiv'>
       <form id="searchforum" action="Project_Update.php" method="post">         
-         <input name="recipename" type="text" value='$rname[recipename]'><?php echo "Recipe Name"; ?></br></br>
+         <input name="recipename" type="text" value='<?php echo $rname[recipename]; ?>'>Recipe Name</br></br>
          <?php
             foreach($items as $item)
             {
