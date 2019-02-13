95c524d441fef6402cbbfd21c2b80b87e1a5eb7a
culigan <culigan@gmail.com>
2019-02-13 16:23:37 -0700

    adding my project

--- web/Project/Project_Update.php
+++ web/Project/Project_Update.php
@@ -2,6 +2,7 @@
    $count = count($_POST);
    $recID = $_POST['size0'];
    $ingredID = $_POST['ingred0'];
+   echo $ingred0;
    //$intrecID =  chop($recID, "item");
    echo $recID;
    $count = ($count - 1) / 3;
