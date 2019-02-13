289e87187a7886df947eecbb597e04b3ca85171f
culigan <culigan@gmail.com>
2019-02-12 21:01:06 -0700

    adding my project

--- web/Project/Project_Edit.php
+++ web/Project/Project_Edit.php
@@ -17,7 +17,15 @@ session_start();
    require('DB_Connect.php');
    $db = connectToDB();
    echo $id;
-   echo $id;
+   $rname = $db->query("SELECT recipename as name FROM recipe where id = " . $id . ";");
+   echo $rname[name];
+    $search = "SELECT ri.id as rid, ri.ingredient as ingredient, ms.measurementsize as msize,";
+    $search .= " mt.measurementname as mtype FROM recipeitems ri join measurementsize ";
+    $search .= "ms on ri.measurementsize_id = ms.id join measurementtype mt on ";
+    $search .= "ri.measurementtype_id = mt.id  where recipe_id = " . $id . ";";
+    echo $search;
+    $items = $db->query($search);
+    echo $id;
     //echo $items[rid];
     echo $id;
    }
@@ -45,15 +53,8 @@ session_start();
    <div id='itemdiv'>
       <form id="searchforum" action="Project_Update.php" method="post">         
          <?php 
-            $rname = $db->query("SELECT recipename FROM recipe where id = " . $id . ";");
-             $search = "SELECT ri.id as rid, ri.ingredient as ingredient, ms.measurementsize as msize,";
-             $search .= " mt.measurementname as mtype FROM recipeitems ri join measurementsize ";
-             $search .= "ms on ri.measurementsize_id = ms.id join measurementtype mt on ";
-             $search .= "ri.measurementtype_id = mt.id  where recipe_id = " . $id . ";";
-             echo $search;
-             $items = $db->query($search);
-    
-            echo "<input name='recipenam' type='text' value='" . $rname[recipename] . "'>Recipe Name</br></br>"; 
+            //echo $results;
+            //echo "<input name='recipenam' type='text' value='$results[recipename]'>Recipe Name</br></br>"; 
          
             foreach($items as $item)
             {
