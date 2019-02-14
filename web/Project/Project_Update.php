c339e50ed787708aba8eee2d148e42a2f7b85565
culigan <culigan@gmail.com>
2019-02-13 23:12:55 -0700

    adding my project

--- web/Project/Project_Update.php
+++ web/Project/Project_Update.php
@@ -14,23 +14,20 @@
    echo $count;
    $count = ($count - 3) / 3;
    echo $count;
-   $stmt = "Update recipe set recipename = :recipename, directions = :direct where id = :id;";
+   $stmt = "Update recipe set recipename = '" . $_POST['recipenam'] . "', directions = '" . $direct . "' where id = " . $recID . ";";
    echo $stmt;
-   $stmt->bindValue(':recipename', $_POST['recipenam']);
-   $stmt->bindValue(':direct', $direct);
-   $stmt->bindValue(':id', $recID);
-   $q = $db->prepare($stmt);
-   $q->execute();
-    /*///$stmt->bindValue(':recipename', $_POST['recipenam']);
-    //$stmt->execute();
+   $q = $db->query($stmt);
+   /*$q->execute();
+    ///$stmt->bindValue(':recipename', $_POST['recipenam']);
+    //$stmt->execute();*/
     echo "here";
     for($i = 0; $i < $count; $i++)
     {
       $ingredStmt = "Update recipeitems Set measurementsize_id = $_POST['size$count'], ";
 	   $ingredStmt .= "measurementtype_id = $_POST['type$count'], ingredient = $_POST['ingred$count'] "
       $ingredStmt .= "Where recipe_id = (Select id from recipe where recipename = $_POST['recipenam']); ";
-      $updateStmt = $db->prepare($ingredStmt);
-      $updateStmt->execute();
+      $updateStmt = $db->query($ingredStmt);
+      //$updateStmt->execute();
     }
 
     echo "<span>Recipe Saved </span>";
