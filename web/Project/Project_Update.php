be698657ae6f002c345f1fb51c46450a75c4903e
culigan <culigan@gmail.com>
2019-02-13 22:21:08 -0700

    adding my project

--- web/Project/Project_Update.php
+++ web/Project/Project_Update.php
@@ -10,7 +10,7 @@
    echo $count;
    $count = ($count - 3) / 3;
    echo $count;
-   //$stmt = "Update recipe recipename, directions set recipename = $_POST['recipenam'], directions = $direct where id = $recid" ;
+   $stmt = "Update recipe recipename, directions set recipename = " . $_POST['recipenam'] . ", directions = " . $direct . " where id = " . $recid ;
    //echo $stmt;
     /*///$stmt->bindValue(':recipename', $_POST['recipenam']);
     //$stmt->execute();
