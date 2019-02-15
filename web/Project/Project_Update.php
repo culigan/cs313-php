78a089f6f60729d872af32b0043375c7d8b15501
culigan <culigan@gmail.com>
2019-02-14 19:12:53 -0700

    adding my project

--- web/Project/Project_Update.php
+++ web/Project/Project_Update.php
@@ -22,7 +22,7 @@
       $tempingred = "ingred" . $i;
       $ingredStmt = "Update recipeitems Set measurementsize_id = $_POST[$temp], ";
 	   $ingredStmt .= "measurementtype_id = $_POST[$temptype], ingredient = '" . $_POST[$tempingred] . "' ";
-      $ingredStmt .= "Where recipe_id = (Select id from recipe where recipename = '" . $_POST['recipenam']) . "'; ";
+      $ingredStmt .= "Where recipe_id = (Select id from recipe where recipename = '" . $_POST['recipenam'] . "'); ";
       $updateStmt = $db->query($ingredStmt);
       //$updateStmt->execute();
     }
