501e708c04503237a02f5bcbbc4eab65ddf1aeb3
culigan <culigan@gmail.com>
2019-02-15 07:06:18 -0700

    adding my project.

--- web/Project/Project_1.php
+++ web/Project/Project_1.php
@@ -11,10 +11,46 @@
    require('DB_Connect.php');
    $db = connectToDB();
 
+   if(isset($_POST['ammount0']))
+   {
+      $name = $_POST['recipename'];
+      $count = count($_POST);
+      $count = ($count - 5) / 3;
+      echo $count;
+      $addSize;
+      $addType;
+      $recipen = $_POST['recipename'];
+      echo $recipen;
+      $direct = $_POST['direct'];
+      echo $direct;
+      $recfood = $_POST['food'];
+      echo $recfood;
+      $reccat = $_POST['mealcat'];
+      echo $reccat;
+      $userID = $db->query("SELECT id FROM user_table where username = $_SESSION['user'];");
+      $userResults = $userID->fetchAll(PDO::FETCH_ASSOC);
+      print_r($userResults);
+      $insertUserID = $db->prepare("Insert Into Recipe (recipename, Directions, FoodType_ID,";
+      $insertUserID .= " mealcategory_id, user_id) Values (:recipename, :directions, :foodtype_id,";
+      $insertUserID .= " :mealcategory_id, :user_id)";
+         $insertUserID->bindValue(':recipename', $recipen);
+         $insertUserID->bindValue(':directions', $direct);
+         $insertUserID->bindValue(':mealcategory_id', $reccat);
+         $insertUserID->bindValue(':foodtype_id', $recfood);
+         $insertUserID->bindValue(':user_id', $userResults[0].[id]);
+         $insertUserID->execute();
+      /*for($i = 0; $i < $count; $i++)
+      {
+         
+      }*/
+   }
+
    try
    {
       $sizes = $db->query("SELECT * FROM measurementsize;");
       $types = $db->query("SELECT * FROM measurementtype;");
+      $foodtype = $db->query("SELECT * FROM foodtype;")
+      $meals = $db->query("SELECT * FROM mealcategory;")
    }
    catch (PDOException $ex)
    {
@@ -46,19 +82,33 @@
          ?>
          </p></br>
             <input name="recipename" type="text" required>Recipe Name</br></br>
-
+            <select id="food" name="food"  required>
+               <? 
+               foreach($foodtype as $food){
+               echo "<option value=$food[id]> $food[categoryname]</option>";
+               }
+               ?>
+            </select>Food Type
+            <select id="mealcat" name="mealcat"  required>
+               <? 
+               foreach($meals as $meal){
+               echo "<option value=$meal[id]> $meal[measurementsize]</option>";
+               }
+               ?>
+            </select>Amount
+            
          <div id="ingred">
             <select id="amount0" name="amount0"  required>
                <? 
                foreach($sizes as $size){
-               echo "<option> $size[measurementsize]</option>";
+               echo "<option value=$size[id]> $size[measurementsize]</option>";
                }
                ?>
             </select>Amount
             <select id="meastype0" name="meastype0" required>
                <? 
                   foreach($types as $type){
-                  echo "<option> $type[measurementname]</option>";
+                  echo "<option value=$type[id]> $type[measurementname]</option>";
                   }
                ?>
             </select>Measurement Type
