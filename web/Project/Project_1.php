2d5d698f05dea3fa4b9818586f9a677756dfe1a8
culigan <culigan@gmail.com>
2019-02-15 20:53:41 -0700

    adding my project.

--- web/Project/Project_1.php
+++ web/Project/Project_1.php
@@ -28,7 +28,7 @@
       echo $reccat;
       $userID = $db->query("SELECT id FROM user_table where username = '" . $_SESSION['user'] . "';");
       
-      print_r($userID);
+      print_r($userID[0]);
       $insertString = "Insert Into Recipe (recipename, Directions, FoodType_ID,";
       $insertString .= " mealcategory_id, user_id) Values (:recipename, :directions, :foodtype_id,";
       $insertString .= " :mealcategory_id, :user_id)";
